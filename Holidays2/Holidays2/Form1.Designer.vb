06a1a122f56fbf2e8289fef2ef7b373797aa5519
CalebBaugh <boffy7@hotmail.com>
2017-03-30 11:14:58 +0100

    Add files via upload

---
+++ Holidays2/Holidays2/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDetails = New System.Windows.Forms.TextBox()
+        Me.txtDOB = New System.Windows.Forms.DateTimePicker()
+        Me.cmdSave = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtEmailAddress = New System.Windows.Forms.TextBox()
+        Me.txtName2 = New System.Windows.Forms.TextBox()
+        Me.txtName1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtClientID = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmdSearch = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtDetails
+        '
+        Me.txtDetails.Location = New System.Drawing.Point(18, 157)
+        Me.txtDetails.Multiline = True
+        Me.txtDetails.Name = "txtDetails"
+        Me.txtDetails.Size = New System.Drawing.Size(260, 59)
+        Me.txtDetails.TabIndex = 47
+        '
+        'txtDOB
+        '
+        Me.txtDOB.CalendarTitleForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtDOB.Location = New System.Drawing.Point(78, 107)
+        Me.txtDOB.Name = "txtDOB"
+        Me.txtDOB.Size = New System.Drawing.Size(200, 20)
+        Me.txtDOB.TabIndex = 46
+        '
+        'cmdSave
+        '
+        Me.cmdSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.cmdSave.Location = New System.Drawing.Point(18, 222)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(96, 32)
+        Me.cmdSave.TabIndex = 45
+        Me.cmdSave.Text = "Save"
+        Me.cmdSave.UseVisualStyleBackColor = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(7, 6)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(106, 29)
+        Me.lblName.TabIndex = 39
+        Me.lblName.Text = "Holidays"
+        '
+        'txtEmailAddress
+        '
+        Me.txtEmailAddress.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtEmailAddress.Location = New System.Drawing.Point(110, 131)
+        Me.txtEmailAddress.Name = "txtEmailAddress"
+        Me.txtEmailAddress.Size = New System.Drawing.Size(112, 20)
+        Me.txtEmailAddress.TabIndex = 38
+        '
+        'txtName2
+        '
+        Me.txtName2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtName2.Location = New System.Drawing.Point(110, 83)
+        Me.txtName2.Name = "txtName2"
+        Me.txtName2.Size = New System.Drawing.Size(112, 20)
+        Me.txtName2.TabIndex = 37
+        '
+        'txtName1
+        '
+        Me.txtName1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtName1.Location = New System.Drawing.Point(110, 59)
+        Me.txtName1.Name = "txtName1"
+        Me.txtName1.Size = New System.Drawing.Size(112, 20)
+        Me.txtName1.TabIndex = 36
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(20, 131)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 13)
+        Me.Label5.TabIndex = 44
+        Me.Label5.Text = "Email Address"
+        '
+        'txtClientID
+        '
+        Me.txtClientID.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtClientID.Location = New System.Drawing.Point(110, 35)
+        Me.txtClientID.Name = "txtClientID"
+        Me.txtClientID.Size = New System.Drawing.Size(112, 20)
+        Me.txtClientID.TabIndex = 35
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 107)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(30, 13)
+        Me.Label4.TabIndex = 43
+        Me.Label4.Text = "DOB"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 83)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 42
+        Me.Label3.Text = "Last Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 41
+        Me.Label2.Text = "First Name"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 40
+        Me.Label1.Text = "ClientID"
+        '
+        'cmdSearch
+        '
+        Me.cmdSearch.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.cmdSearch.Location = New System.Drawing.Point(182, 222)
+        Me.cmdSearch.Name = "cmdSearch"
+        Me.cmdSearch.Size = New System.Drawing.Size(96, 32)
+        Me.cmdSearch.TabIndex = 34
+        Me.cmdSearch.Text = "Search"
+        Me.cmdSearch.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.txtDetails)
+        Me.Controls.Add(Me.txtDOB)
+        Me.Controls.Add(Me.cmdSave)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.txtEmailAddress)
+        Me.Controls.Add(Me.txtName2)
+        Me.Controls.Add(Me.txtName1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtClientID)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmdSearch)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtDetails As System.Windows.Forms.TextBox
+    Friend WithEvents txtDOB As System.Windows.Forms.DateTimePicker
+    Friend WithEvents cmdSave As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtEmailAddress As System.Windows.Forms.TextBox
+    Friend WithEvents txtName2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtName1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtClientID As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Private WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmdSearch As System.Windows.Forms.Button
+
+End Class
---
+++ Holidays2/Holidays2/Form1.vb
@@ -0,0 +1,93 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Structure CustomerData
+        Public Client As String
+        Public First As String
+        Public Second As String
+        Public DateBirth As Date
+        Public Email As String
+    End Structure
+
+    Private Function ValidateCheck(ByVal CheckField As String, ByVal CheckString As String, ByVal MaximumLength As Integer) As String
+        If CheckString = "" Then
+            MessageBox.Show("Please enter a value in " & CheckField)
+        ElseIf CheckString > MaximumLength Then
+            MessageBox.Show("The information entered in " & CheckField & " is either too long or too short but it must be less than " & MaximumLength & "words long")
+        ElseIf CheckField = "Email" And (CheckString.Contains("@") = False Or CheckString.Contains(".") = False) Then
+            MessageBox.Show("The email is not valid please enter a valid email address")
+        End If
+        Return ""
+    End Function
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        txtClientID.Enabled = False
+
+    End Sub
+    Private Sub txtName1_KeyDown(sender As Object, e As PreviewKeyDownEventArgs) Handles txtName1.PreviewKeyDown
+        Select Case e.KeyValue
+            Case Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9, Keys.D0, Keys.ControlKey, Keys.NumPad1, Keys.NumPad2, Keys.NumPad3, Keys.NumPad4, Keys.NumPad5, Keys.NumPad6, Keys.NumPad7, Keys.NumPad8, Keys.NumPad9, Keys.NumPad0
+                SendKeys.Send("{BACKSPACE}")
+        End Select
+    End Sub
+
+    Private Sub txtName2_KeyDown(sender As Object, e As KeyEventArgs) Handles txtName2.KeyDown
+        Select Case e.KeyValue
+            Case Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9, Keys.D0, Keys.ControlKey, Keys.NumPad1, Keys.NumPad2, Keys.NumPad3, Keys.NumPad4, Keys.NumPad5, Keys.NumPad6, Keys.NumPad7, Keys.NumPad8, Keys.NumPad9, Keys.NumPad0
+                SendKeys.Send("{BACKSPACE}")
+        End Select
+    End Sub
+
+    Private Sub cmdSearch_Click(sender As System.Object, e As System.EventArgs) Handles cmdSearch.Click
+
+        txtClientID.Enabled = True
+
+        MessageBox.Show("Please enter the Client ID you would like to search for")
+        txtClientID.Text = ""
+        txtName1.Text = ""
+        txtName2.Text = ""
+        txtEmailAddress.Text = ""
+        txtDOB.Text = ""
+        txtClientID.Enabled = True
+        txtName1.Enabled = False
+        txtName2.Enabled = False
+        txtEmailAddress.Enabled = False
+        txtDOB.Enabled = False
+        Dim CustomerDetails As String = "E:\Computing Tasks\Candidate\Holidays\CustomerData.text"
+
+        Dim TextLine As String
+
+        If System.IO.File.Exists(CustomerDetails) = True Then
+
+        End If
+
+        Dim objReader As New System.IO.StreamReader(CustomerDetails)
+
+        Do While objReader.Peek() <> -1
+
+            TextLine = TextLine & objReader.ReadLine() & vbNewLine
+
+        Loop
+
+        txtDetails.Text = TextLine
+    End Sub
+
+    Private Sub cmdSave_Click(sender As System.Object, e As System.EventArgs) Handles cmdSave.Click
+        Dim CustomerData As String = "E:\Computing Tasks\Candidate\Holidays\CustomerData.text"
+
+        If System.IO.File.Exists(CustomerData) = True Then
+
+        End If
+
+        Dim objWriter As New System.IO.StreamWriter(CustomerData)
+
+        objWriter.Write(txtName1.Text)
+        objWriter.Write(" ")
+        objWriter.Write(txtName2.Text)
+        objWriter.Write(" ")
+        objWriter.Write(txtEmailAddress.Text)
+        objWriter.Write(" ")
+        objWriter.Write(txtDOB.Text)
+        objWriter.Close()
+    End Sub
+End Class
